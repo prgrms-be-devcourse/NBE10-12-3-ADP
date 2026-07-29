304f2d43400723ad3204bb439a5c63df6c5c5974
EasyH <skysea001010@gmail.com>
2026-07-29 17:49:26 +0900

    refactor: rq kotlin으로 변환

---
+++ back/src/main/java/com/back/global/rq/Rq.kt
@@ -0,0 +1,121 @@
+package com.back.global.rq
+
+import com.back.domain.member.entity.Member
+import com.back.domain.member.entity.Role
+import com.back.domain.member.service.MemberService
+import com.back.global.security.SecurityUser
+import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+import java.util.*
+import java.util.function.Function
+import java.util.function.Predicate
+
+@Component
+class Rq(
+    private val req: HttpServletRequest,
+    private val resp: HttpServletResponse,
+    private val memberService: MemberService
+
+) {
+
+    @Value($$"${custom.security.cookieSecure}")
+    private val isSecure = false
+
+    @Value($$"${custom.security.cookieDomain}")
+    private val cookieDomain: String? = null
+
+    val actor: Member?
+        get() = Optional.ofNullable<Authentication?>(
+            SecurityContextHolder
+                .getContext()
+                .authentication
+        )
+            .map<Any?>(Function { obj: Authentication? -> obj!!.principal })
+            .filter { principal: Any? -> principal is SecurityUser }
+            .map(Function { principal: Any? -> principal as SecurityUser })
+            .map(Function { securityUser: SecurityUser? ->
+                Member(
+                    securityUser.id,
+                    securityUser!!.username,
+                    securityUser.name,
+                    if (hasAdminAuthority(securityUser.authorities)) Role.ADMIN else Role.USER
+                )
+            })
+            .orElse(null)
+
+    private fun hasAdminAuthority(authorities: MutableCollection<out GrantedAuthority?>): Boolean {
+        return authorities.stream()
+            .anyMatch { authority: GrantedAuthority? -> "ROLE_ADMIN" == authority!!.authority }
+    }
+
+    fun getHeader(name: String?, defaultValue: String?): String? {
+        return Optional
+            .ofNullable<String?>(req.getHeader(name))
+            .filter(Predicate { headerValue: String? -> !headerValue!!.isBlank() })
+            .orElse(defaultValue)
+    }
+
+    fun setHeader(name: String?, value: String?) {
+        var value = value
+        if (value == null) value = ""
+
+        if (value.isBlank()) {
+            req.removeAttribute(name)
+        } else {
+            resp.setHeader(name, value)
+        }
+    }
+
+    fun getCookieValue(name: String, defaultValue: String?): String? {
+        return Arrays.stream<Cookie?>(
+            Optional.ofNullable<Array<Cookie?>?>(req.cookies).orElse(arrayOfNulls(0))
+        )
+            .filter { cookie: Cookie? -> name == cookie!!.name }
+            .map { obj: Cookie? -> obj!!.value }
+            .filter { value: String? -> !value.isNullOrBlank() }
+            .findFirst()
+            .orElse(defaultValue)
+    }
+
+    fun setCookie(name: String?, value: String?, maxAge: Int) {
+        var value = value
+        if (value == null) value = ""
+
+        val cookie = Cookie(name, value)
+        cookie.path = "/"
+        cookie.isHttpOnly = true
+        cookie.domain = cookieDomain
+        cookie.secure = isSecure
+        cookie.setAttribute("SameSite", if (isSecure) "None" else "Strict")
+
+        if (value.isBlank()) cookie.maxAge = 0
+        else cookie.maxAge = maxAge
+
+        resp.addCookie(cookie)
+    }
+
+    fun setCookie(name: String?, value: String?) {
+        setCookie(name, value, 60 * 60 * 24 * 365)
+    }
+
+    fun deleteCookie(name: String?) {
+        setCookie(name, null)
+    }
+
+    fun sendRedirect(url: String?) {
+        resp.sendRedirect(url)
+    }
+
+    val actorFromDb: Member?
+        get() {
+            val actor = this.actor ?: return null
+
+            return memberService.findById(actor.id)
+        }
+}
