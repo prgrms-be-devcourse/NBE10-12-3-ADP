c7fff2402f8618672743abf4ca6689e84f60096c
EasyH <skysea001010@gmail.com>
2026-07-30 17:16:16 +0900

    [fix/ADP-179] Gemini 제안의 잘못된 코드 변경 복구 및 패키지명 kotlin으로 수정 (#16)

--- back/src/main/kotlin/com/back/global/security/SecurityUser.kt
+++ back/src/main/kotlin/com/back/global/security/SecurityUser.kt
@@ -7,19 +7,11 @@ import org.springframework.security.oauth2.core.user.OAuth2User
 class SecurityUser(
     val id: Long,
     username: String,
-    name: String,
-    authorities: Collection<GrantedAuthority>
-) : User(username, "", authorities), OAuth2User {
-    private val _name = name
-
-class SecurityUser(
-    val id: Long,
-    username: String,
-    override val name: String,
+    private val _name: String,
     authorities: Collection<GrantedAuthority>
 ) : User(username, "", authorities), OAuth2User {
 
     override fun getAttributes() = mapOf<String, Any>()
-}
     override fun getName() = _name
+
 }
