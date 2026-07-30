497ab9d92967d925300612aef3052ba2aeb7e58e
puppywimy <eric250@naver.com>
2026-07-30 12:09:19 +0900

    refactor: WidgetControllerV2 kotlin으로 변환

--- back/src/main/java/com/back/domain/widget/controller/WidgetControllerV2.kt
+++ back/src/main/java/com/back/domain/widget/controller/WidgetControllerV2.kt
@@ -1,26 +1,26 @@
 package com.back.domain.widget.controller
 
-import com.back.domain.widget.service.WidgetService
+import com.back.domain.widget.service.WidgetByWidgetServerService
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus.OK
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.http.HttpStatus.OK
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-@RequestMapping("/api/v1/widgets")
-class ApiV1WidgetController(
-    private val widgetService: WidgetService
+@RequestMapping("/api/v2/widgets")
+class WidgetControllerV2(
+    private val widgetByWidgetServerService: WidgetByWidgetServerService
 ) {
 
     @GetMapping("{githubId}")
     fun getWidget(
         @PathVariable githubId: String
     ): ResponseEntity<String> {
-        val widgetString = widgetService.createWidget(githubId)
+        val widgetString = widgetByWidgetServerService.createWidget(githubId)
         val httpHeaders = HttpHeaders().apply {
             contentType = MediaType.parseMediaType("image/svg+xml")
         }
