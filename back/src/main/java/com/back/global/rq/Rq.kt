68b02813ca033c9b5c3c4f6fc10ee24b4a17d03f
EasyH <skysea001010@gmail.com>
2026-07-30 12:08:39 +0900

    refactor: Rq 코드 간소화

--- back/src/main/java/com/back/global/rq/Rq.kt
+++ back/src/main/java/com/back/global/rq/Rq.kt
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
-import java.util.*
 import java.util.function.Predicate
 
 @Component
@@ -40,16 +39,12 @@ class Rq(
             }
 
 
-    private fun hasAdminAuthority(authorities: MutableCollection<out GrantedAuthority?>): Boolean {
-        return authorities.stream()
-            .anyMatch { authority: GrantedAuthority? -> "ROLE_ADMIN" == authority!!.authority }
+    private fun hasAdminAuthority(authorities: Collection<GrantedAuthority>): Boolean {
+        return authorities.any { authority -> "ROLE_ADMIN" == authority.authority }
     }
 
-    fun getHeader(name: String?, defaultValue: String?): String? {
-        return Optional
-            .ofNullable<String?>(req.getHeader(name))
-            .filter(Predicate { headerValue: String? -> !headerValue!!.isBlank() })
-            .orElse(defaultValue)
+    fun getHeader(name: String, defaultValue: String): String {
+        return req.getHeader(name) ?: defaultValue
     }
 
     fun setHeader(name: String?, value: String?) {
@@ -62,16 +57,12 @@ class Rq(
         }
     }
 
-    fun getCookieValue(name: String, defaultValue: String?): String? {
-        return Arrays.stream<Cookie?>(
-            Optional.ofNullable<Array<Cookie?>?>(req.cookies).orElse(arrayOfNulls(0))
-        )
-            .filter { cookie: Cookie? -> name == cookie!!.name }
-            .map { obj: Cookie? -> obj!!.value }
-            .filter { value: String? -> !value.isNullOrBlank() }
-            .findFirst()
-            .orElse(defaultValue)
-    }
+    fun getCookieValue(name: String, defaultValue: String): String =
+        req.cookies
+            ?.firstOrNull {it.name == name}
+            ?.value
+            ?.takeIf {it.isNotBlank()}
+            ?: defaultValue
 
     fun setCookie(name: String?, value: String?, maxAge: Int) {
         var value = value
