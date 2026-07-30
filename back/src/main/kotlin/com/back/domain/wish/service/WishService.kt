86d85f2efc60ca7ab572f92d947510b799519540
EasyH <skysea001010@gmail.com>
2026-07-30 16:11:52 +0900

    fix: 찜 삭제 도서 id 이용 -> 찜 id 이용으로 수정, 기존은 레거시로 변경

--- back/src/main/kotlin/com/back/domain/wish/service/WishService.kt
+++ back/src/main/kotlin/com/back/domain/wish/service/WishService.kt
@@ -7,7 +7,6 @@ import com.back.domain.wish.repository.WishRepository
 import com.back.global.exception.ServiceException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.util.Optional
 
 @Service
 @Transactional(readOnly = true)
@@ -16,21 +15,33 @@ class WishService(
 ) {
     fun findAll(): List<Wish> = wishRepository.findAll()
 
-    fun findByMember(member: Member): List<Wish> = wishRepository.findByMember(member)
-
-    private fun findByMemberAndBook(member: Member, book: Book): Optional<Wish> =
-        wishRepository.findByMemberAndBook(member, book)
+    fun getWishesByMember(member: Member): List<Wish> = wishRepository.findByMember(member)
 
     fun addWish(member: Member, book: Book): Wish {
-        if (findByMemberAndBook(member, book).isPresent) {
+        if (wishRepository.findByMemberAndBook(member, book).isPresent) {
             throw ServiceException("409-1", "이미 존재하는 찜 정보입니다.")
         }
         return wishRepository.save(Wish(member, book))
     }
 
-    fun deleteWish(member: Member, book: Book) {
-        val wish = findByMemberAndBook(member, book)
+    fun oldDeleteWish(member:Member, book: Book) {
+        val wish = wishRepository.findByMemberAndBook(member, book)
             .orElseThrow { ServiceException("404", "존재하지 않는 찜 정보입니다.") }
+
+        if (member != wish.member)
+            throw ServiceException("403-1", "찜 삭제 권한이 없습니다.")
+
+        wishRepository.delete(wish)
+
+    }
+
+    fun deleteWish(member: Member, id: Long) {
+        val wish = wishRepository.findById(id)
+            .orElseThrow { ServiceException("404-1", "존재하지 않는 찜 정보입니다.") }
+
+        if (member != wish.member)
+            throw ServiceException("403-1", "찜 삭제 권한이 없습니다.")
+
         wishRepository.delete(wish)
     }
 }
