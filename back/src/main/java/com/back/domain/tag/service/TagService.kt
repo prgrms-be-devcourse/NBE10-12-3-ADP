587eaf568cd8feb53b4efe1002fb961f1ccffc90
puppywimy <eric250@naver.com>
2026-07-30 11:59:17 +0900

    refactor: TagService kotlin으로 변환

---
+++ back/src/main/java/com/back/domain/tag/service/TagService.kt
@@ -0,0 +1,26 @@
+package com.back.domain.tag.service
+
+import com.back.domain.tag.entity.Tag
+import com.back.domain.tag.repository.TagRepository
+import com.back.global.exception.ServiceException
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class TagService(
+    private val tagRepository: TagRepository
+) {
+
+    @Transactional
+    fun post(name: String) {
+        if (tagRepository.findByName(name) != null)
+            throw ServiceException("409-1", "이미 존재하는 태그입니다.")
+
+        tagRepository.save(Tag(name))
+    }
+
+    @Transactional
+    fun findByNameOrSave(name: String): Tag =
+        tagRepository.findByName(name) ?: tagRepository.save(Tag(name))
+}
