804a5f527e02bceee9e8a672a5512f6d5574ea80
puppywimy <eric250@naver.com>
2026-07-30 16:09:11 +0900

    fix: SecurityUser getName 추상 메서드 구현과 필드의 Getter 충돌

--- back/src/main/java/com/back/global/security/SecurityUser.kt
+++ back/src/main/java/com/back/global/security/SecurityUser.kt
@@ -7,10 +7,11 @@ import org.springframework.security.oauth2.core.user.OAuth2User
 class SecurityUser(
     val id: Long,
     username: String,
-    val name: String,
+    name: String,
     authorities: Collection<GrantedAuthority>
 ) : User(username, "", authorities), OAuth2User {
+    private val _name = name
 
     override fun getAttributes() = mapOf<String, Any>()
-    override fun getName() = name
+    override fun getName() = _name
 }
