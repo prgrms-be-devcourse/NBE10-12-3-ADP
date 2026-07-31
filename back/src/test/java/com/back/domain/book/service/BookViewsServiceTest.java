f02dbe75e180501231074b998c512e0849228ded
EasyH <skysea001010@gmail.com>
2026-07-31 11:50:29 +0900

    [fix/ADP-176] 테스트를 통과하도록 메인 코드 수정 (#25)

--- back/src/test/java/com/back/domain/book/service/BookViewsServiceTest.java
+++ back/src/test/java/com/back/domain/book/service/BookViewsServiceTest.java
@@ -1,6 +1,7 @@
 package com.back.domain.book.service;
 
 import com.back.domain.book.dto.BookDto;
+import com.back.domain.book.entity.Book;
 import com.back.domain.review.service.ReviewService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,32 +23,34 @@ public class BookViewsServiceTest {
     @Autowired
     private BookService bookService;
 
-//    @Test
-//    @DisplayName("조회수 기반 도서 순위 조회")
-//    void t1() {
-//
-//        List<Integer> viewsCount = List.of(3, 5, 1);
-//
-//        for (int i = 0; i < viewsCount.size(); i++) {
-//            for (int j = 0; j < viewsCount.get(i); j++) {
-//                bookViewsService.incrementViewCount((long)(i + 1));
-//            }
-//        }
-//
-//        List<BookDto> bookRank = bookViewsService.topViewedInLastHour(0, 0);
-//
-//        int upperCnt = Integer.MAX_VALUE;
-//
-//        for (int i = 0; i < bookRank.size(); i++) {
-//            long id = bookRank.get(i).id();
-//            int nowCnt = bookViewsService.getViewCount(id);
-//
-//            assertThat(nowCnt).isEqualTo(viewsCount.get((int)(id - 1)));
-//            assertThat(upperCnt).isGreaterThanOrEqualTo(nowCnt);
-//
-//            upperCnt = nowCnt;
-//
-//        }
-//
-//    }
+    @Test
+    @DisplayName("조회수 기반 도서 순위 조회")
+    void t1() {
+
+        List<Integer> viewsCount = List.of(3, 5, 1);
+
+        for (int i = 0; i < viewsCount.size(); i++) {
+            for (int j = 0; j < viewsCount.get(i); j++) {
+                Book book = bookService.getBookById(i + 1);
+                bookService.incrementViewCount(book);
+            }
+        }
+
+        List<Book> bookRank = bookService.getBooksOrderByRank("views", 0, 10);
+
+        int upperCnt = Integer.MAX_VALUE;
+
+        for (Book book : bookRank) {
+            int nowCnt = bookService.getBookViewCount(book);
+
+            System.out.println("view: " + nowCnt);
+
+            assertThat(nowCnt).isEqualTo(viewsCount.get((int) (book.getId() - 1)));
+            assertThat(upperCnt).isGreaterThanOrEqualTo(nowCnt);
+
+            upperCnt = nowCnt;
+
+        }
+
+    }
 }
