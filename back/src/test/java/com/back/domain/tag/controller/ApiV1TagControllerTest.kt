0cd9aad15cf4337af0074a5d1edbd2702b5c492d
puppywimy <eric250@naver.com>
2026-07-31 16:19:46 +0900

    refactor: ApiV1TagControllerTest kotlin으로 변환

---
+++ back/src/test/java/com/back/domain/tag/controller/ApiV1TagControllerTest.kt
@@ -0,0 +1,56 @@
+package com.back.domain.tag.controller
+
+import com.back.domain.tag.service.TagService
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
+import org.springframework.http.MediaType
+import org.springframework.security.test.context.support.WithUserDetails
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.transaction.annotation.Transactional
+
+@ActiveProfiles("test")
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+class ApiV1TagControllerTest {
+
+    @Autowired
+    private lateinit var mvc: MockMvc
+
+    @Autowired
+    private lateinit var tagService: TagService
+
+    @Test
+    @DisplayName("태그 생성")
+    @WithUserDetails("user1")
+    fun t1() {
+        val resultActions = mvc
+            .perform(
+                post("/api/v1/tags")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(
+                        """
+                        {
+                            "name": "c"
+                        }
+                        """.trimIndent()
+                    )
+            )
+            .andDo(print())
+
+        resultActions
+            .andExpect(handler().handlerType(ApiV1TagController::class.java))
+            .andExpect(status().isCreated)
+            .andExpect(jsonPath("$.resultCode").value("201-1"))
+            .andExpect(jsonPath("$.message").value("태그 생성 성공"))
+    }
+}
