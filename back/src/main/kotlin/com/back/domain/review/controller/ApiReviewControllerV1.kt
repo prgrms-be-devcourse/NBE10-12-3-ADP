fb8e2fc00cf57a0c33da8734219713107f05b68e
mjkim12345 <zmdl96@gmail.com>
2026-08-03 14:25:38 +0900

    refactor: 도서별 리뷰 조회 API summary 명명 규칙에 맞게 수정

--- back/src/main/kotlin/com/back/domain/review/controller/ApiReviewControllerV1.kt
+++ back/src/main/kotlin/com/back/domain/review/controller/ApiReviewControllerV1.kt
@@ -32,7 +32,7 @@ class ApiReviewControllerV1(
 ) {
 
     @GetMapping("/book/{bookId}")
-    @Operation(summary = "리뷰 다건 조회")
+    @Operation(summary = "도서별 리뷰 다건 조회")
     fun getReviewsByBookId(
         @PathVariable bookId: Long
     ): List<ReviewDto> =
