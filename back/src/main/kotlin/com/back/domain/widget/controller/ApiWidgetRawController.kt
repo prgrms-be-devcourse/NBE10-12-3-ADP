f87f5aa8f2024063d9dbbf3dd3411adf70b999b2
EasyH <skysea001010@gmail.com>
2026-08-03 17:10:12 +0900

    fix: request mapping 관련 오류 및 security config 수정

--- back/src/main/kotlin/com/back/domain/widget/controller/ApiWidgetRawController.kt
+++ back/src/main/kotlin/com/back/domain/widget/controller/ApiWidgetRawController.kt
@@ -4,15 +4,17 @@ import com.back.domain.widget.service.WidgetInformationService
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/api/*/")
+@RestController
+@RequestMapping("/api/*/widgets")
 @Tag(name = "ApiWidgetRawController", description = "API 위젯 기초 정보 컨트롤러")
 class ApiWidgetRawController(
     private val widgetInformationService: WidgetInformationService
 ) {
 
-    @GetMapping("{githubId}/raw")
+    @GetMapping("/{githubId}/raw")
     fun getWidgetInformation(
         @PathVariable githubId: String
     ) = widgetInformationService.getWidgetInformation(githubId)
