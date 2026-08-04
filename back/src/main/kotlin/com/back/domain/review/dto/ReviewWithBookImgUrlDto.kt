cf3536ee79b5ea3c6842ba4e652a4ca8f70d527e
EasyH <skysea001010@gmail.com>
2026-08-04 11:21:44 +0900

    fix: dto 관련 피드백 반영 수정

--- back/src/main/kotlin/com/back/domain/review/dto/ReviewWithBookImgUrlDto.kt
+++ back/src/main/kotlin/com/back/domain/review/dto/ReviewWithBookImgUrlDto.kt
@@ -1,30 +1,8 @@
 package com.back.domain.review.dto
 
-import com.back.domain.member.dto.MemberDto
 import com.back.domain.review.entity.Review
-import java.time.LocalDateTime
 
-class ReviewWithBookImgUrlDto(
-    id: Long,
-    bookId: Long,
-    bookTitle: String,
-    rating: Float,
-    content: String,
-    modifiedDate: LocalDateTime,
-    createdDate: LocalDateTime,
-    reviewer: MemberDto,
-    tags: List<String>,
-    val bookImgUrl: String?
-) : ReviewDto(id, bookId, bookTitle, rating, content, modifiedDate, createdDate, reviewer, tags) {
-    constructor(review: Review) : this(
-        review.id,
-        review.book.id,
-        review.book.title,
-        review.rating,
-        review.content,
-        review.modifiedDate,
-        review.createdDate,
-        MemberDto(review.reviewer),
-        review.tags,
-        review.book.imgUrl)
+class ReviewWithBookImgUrlDto(review: Review)
+    : ReviewDto(review) {
+    val bookImgUrl : String? = review.book.imgUrl
 }
