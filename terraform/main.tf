3238740f6e635bfe5461eae15179137cc627f710
puppywimy <eric250@naver.com>
2026-08-04 15:29:39 +0900

    fix: nginx를 위한 sg 규칙 수정

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,18 +58,39 @@ resource "aws_route_table_association" "association_1" {
 
 resource "aws_security_group" "ec2_sg_1" {
   name = "${var.prefix}-ec2-sg-1"
+
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 81
+    to_port     = 81
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   vpc_id = aws_vpc.vpc_1.id
 
   tags = {
