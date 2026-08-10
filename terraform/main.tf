25b96719d5e2498d6e3ed09f307a5a59c3725a95
puppywimy <eric250@naver.com>
2026-08-11 00:45:17 +0900

    feat: EC2 인스턴스 역할에 AmazonS3FullAccess 정책 추가

--- terraform/main.tf
+++ terraform/main.tf
@@ -114,6 +114,11 @@ resource "aws_iam_role" "ec2_role_1" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "s3_full_access" {
+  role       = aws_iam_role.ec2_role_1.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
 resource "aws_iam_role_policy_attachment" "ec2_ssm" {
   role       = aws_iam_role.ec2_role_1.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
